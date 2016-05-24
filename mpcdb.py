5b726c104582aeab83e619d0120b77f8be68194a
SEAP UDEA <seapudea@gmail.com>
2016-05-23 23:20:23 -0500

    Commit

--- mpcdb.py
+++ mpcdb.py
@@ -24,14 +24,18 @@ class dict2obj(object):
         return self
 
 def mysqlSelect(selection="*",table="Bodies",condition=""):
-    DB.execute("show columns from %s"%table)
-    fields=DB.fetchall()
-    
+
+    #QUERY
     sql="select %s from %s %s"%(selection,table,condition)
     DB.execute(sql)
     results=DB.fetchall()
 
-    if selection!="*":
+    if selection=="*":
+        #RECOVER ALL COLUMNS IN TABLE
+        DB.execute("show columns from %s"%table)
+        fields=DB.fetchall()
+    else:
+        #ONLY COLUMNS IN SELECTION
         fields=[(field,) for field in selection.split(",")]
 
     dresults=[]
@@ -43,43 +47,3 @@ def mysqlSelect(selection="*",table="Bodies",condition=""):
         dresults+=[row]
     return dresults
     
-def loadDatabase(server='localhost',
-                 user=USER,
-                 password=PASSWORD,
-                 database=DATABASE):
-    con=mdb.connect(server,user,password,database)
-    with con:
-        dbdict=dict()
-        db=con.cursor()
-        db.execute("show tables;")
-        tables=db.fetchall()
-        for table in tables:
-            table=table[0]
-            dbdict[table]=dict()
-            
-            db.execute("show columns from %s;"%table)
-            fields=db.fetchall()
-            dbdict[table]['fields']=[]
-            for field in fields:
-                fieldname=field[0]
-                fieldtype=field[3]
-                dbdict[table]['fields']+=[fieldname]
-                if fieldtype=='PRI':
-                    dbdict[table]['primary']=fieldname
-
-            db.execute("select * from %s;"%table)
-            rows=db.fetchall()
-
-            dbdict[table]['rows']=dict()
-            for row in rows:
-                rowdict=dict()
-                i=0
-                for field in dbdict[table]['fields']:
-                    rowdict[field]=row[i]
-                    if field==dbdict[table]['primary']:
-                        primary=str(row[i]).strip()
-                    i+=1
-                dbdict[table]['rows'][primary]=rowdict
-
-    return dbdict,con
-
